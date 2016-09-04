f9e74821fa5ac0682db789283369e720dd42d80f
Arpit Malani <malaniarpit@gmail.com>
2016-09-04 11:02:25 +0530

    Second Commit

--- app/app.component.js
+++ app/app.component.js
@@ -11,11 +11,14 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = 'app works!';
     }
     AppComponent = __decorate([
         core_1.Component({
-            selector: 'my-app',
-            templateUrl: './app/app.component.html'
+            moduleId: module.id,
+            selector: 'app-root',
+            templateUrl: 'app.component.html',
+            styleUrls: ['app.component.css']
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.spec.js
+++ app/app.component.spec.js
@@ -1,29 +1,16 @@
-"use strict";
 /* tslint:disable:no-unused-variable */
-var app_component_1 = require('./app.component');
+"use strict";
 var testing_1 = require('@angular/core/testing');
-var platform_browser_1 = require('@angular/platform-browser');
-////////  SPECS  /////////////
-/// Delete this
-describe('Smoke test', function () {
-    it('should run a passing test', function () {
-        expect(true).toEqual(true, 'should pass');
-    });
-});
-describe('AppComponent with TCB', function () {
+var app_component_1 = require('./app.component');
+describe('App: O5BusinessContact', function () {
     beforeEach(function () {
-        testing_1.TestBed.configureTestingModule({ declarations: [app_component_1.AppComponent] });
+        testing_1.addProviders([app_component_1.AppComponent]);
     });
-    it('should instantiate component', function () {
-        var fixture = testing_1.TestBed.createComponent(app_component_1.AppComponent);
-        expect(fixture.componentInstance instanceof app_component_1.AppComponent).toBe(true, 'should create AppComponent');
-    });
-    it('should have expected <h1> text', function () {
-        var fixture = testing_1.TestBed.createComponent(app_component_1.AppComponent);
-        fixture.detectChanges();
-        var h1 = fixture.debugElement.query(function (el) { return el.name === 'h1'; }).nativeElement; // it works
-        h1 = fixture.debugElement.query(platform_browser_1.By.css('h1')).nativeElement; // preferred
-        expect(h1.innerText).toMatch(/angular 2 app/i, '<h1> should say something about "Angular 2 App"');
-    });
+    it('should create the app', testing_1.inject([app_component_1.AppComponent], function (app) {
+        expect(app).toBeTruthy();
+    }));
+    it('should have as title \'app works!\'', testing_1.inject([app_component_1.AppComponent], function (app) {
+        expect(app.title).toEqual('app works!');
+    }));
 });
 //# sourceMappingURL=app.component.spec.js.map
